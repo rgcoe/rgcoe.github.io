dd73bb230468087351d89de72b78813f1a7b6900
rgcoe <rcoe@sandia.gov>
2025-09-26 14:23:54 +0000

    Deploying to gh-pages from @ rgcoe/rgcoe.github.io@0fa5913b04fca0e87ae2ebf0c3d27b00ef9756dc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -48,7 +48,7 @@ ninja.data = [{
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
+              window.location.href = "/books/the_godfather.html";
             },},{id: "news-after-many-years-of-wonderful-work-at-sandia-national-labs-i-will-be-moving-to-virginia-tech-in-january-2026",
           title: 'After many years of wonderful work at Sandia National Labs, I will be...',
           description: "",
@@ -56,52 +56,52 @@ ninja.data = [{
           title: 'Reproducible papers',
           description: "making your work more useful for others (and the future you)",
           section: "Notes",handler: () => {
-              window.location.href = "/notes/1_note/";
+              window.location.href = "/notes/1_note.html";
             },},{id: "notes-latex-best-practices",
           title: 'LaTeX best-practices',
           description: "write and collaborate more cleanly",
           section: "Notes",handler: () => {
-              window.location.href = "/notes/3_note/";
+              window.location.href = "/notes/3_note.html";
             },},{id: "projects-pioneer-wec",
           title: 'Pioneer WEC',
           description: "wave energy for ocean observing",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
+              window.location.href = "/projects/1_project.html";
             },},{id: "projects-wec-spar",
           title: 'WEC-spar',
           description: "wave energy on an ocean observing spar buoy",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
+              window.location.href = "/projects/2_project.html";
             },},{id: "projects-extreme-conditions-modeling",
           title: 'Extreme conditions modeling',
           description: "designing for the 100-year wave",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
+              window.location.href = "/projects/3_project.html";
             },},{id: "projects-foswec",
           title: 'FOSWEC',
           description: "floating oscillating surge wave energy converter",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
+              window.location.href = "/projects/4_project.html";
             },},{id: "projects-siweed",
           title: 'SIWEED',
           description: "Sandia integrated wave energy educational display",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
+              window.location.href = "/projects/5_project.html";
             },},{id: "projects-wec-co-design",
           title: 'WEC co-design',
           description: "holistic design of wave energy converters",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
+              window.location.href = "/projects/6_project.html";
             },},{id: "projects-wavebot",
           title: 'WaveBot',
           description: "fundamental WEC dynamics and controls",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
+              window.location.href = "/projects/8_project.html";
             },},{id: "projects-swept-lab",
           title: 'SWEPT Lab',
           description: "Sandia wave energy power take-off lab",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/9_project.html";
             },},{
         id: 'social-github',
         title: 'GitHub',
@@ -144,30 +144,4 @@ ninja.data = [{
         handler: () => {
           window.open("https://www.scopus.com/authid/detail.uri?authorId=55532590700", "_blank");
         },
-      },{
-      id: 'light-theme',
-      title: 'Change theme to light',
-      description: 'Change the theme of the site to Light',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("light");
-      },
-    },
-    {
-      id: 'dark-theme',
-      title: 'Change theme to dark',
-      description: 'Change the theme of the site to Dark',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("dark");
-      },
-    },
-    {
-      id: 'system-theme',
-      title: 'Use system default theme',
-      description: 'Change the theme of the site to System Default',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("system");
-      },
-    },];
+      },];
